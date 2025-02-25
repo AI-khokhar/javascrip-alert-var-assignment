fd614870adab4d9a64fb7b90ae8728edde492bcf
Asiya Ikhlaq <ikhlaqasiya7@gmail.com>
2025-02-25 09:21:59 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+var subject ={
+periodone : "English",
+periodtwo : "Math",
+periodthreee : "Urdu",
+periodfour : "Islamiyat",
+periodfive : "science"
+}
+var marketproducts ={
+    forhair : "Shampoo",
+    forskin : "Lotion",
+    forface : "Cream",
+    fornalis : "Nailpolish",
+    forfinger : "Rings"
+}
+var food = {
+sweet : "ice cream",
+spaciy : "Biryani",
+chili : "karaihi",
+cold : "cold water",
+hot : "Coffe"
+}
+var cloths = {
+    ledies : "three pice",
+    grnts : "kote and pantes",
+    babygirls : "two pice",
+    babaayboby : "shorts ant shirts",
+    size : "all types of size"
+}
+var sale = {
+    pant : "34$",
+    shirts : "23$",
+    kote : "15$",
+    threepice : "56$",
+    tity : "12$"
+}
+var stationary = {
+    forwrite : "pencil",
+    forcolor : "color pencil",
+    forsharp : "sharpner",
+    foreraze : "remover",
+    fornote : "note pade"
+}
+var falgs ={
+    pakistanFlagColor : "green and white",
+    chaniflagcolor : "red",
+    afganistanflagcolor : "red,white and green",
+    indainflagcolor : "orange,white and green"
+}
+var makeup = {
+    forFace : "poder",
+    forEyes : "eyeslaishiz",
+    forlips : "lipstick",
+    forhands : "watch"
+}
+var car = {
+    price : "340$",
+    color : "black",
+    varsion : "carolla",
+    size : "4500"
+}
+var daysplan={
+    monday: "work day",
+    tuesday: "extra work day",
+    wednesday: "same as monady and tuesday",
+    staturday: "hoily day of empoly",
+    sunday : "week end of woking week"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
